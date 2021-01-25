102dc8781f46e3f50f0ec7497066dd8b830fc7ae
junY97 <whwnsdud2024@naver.com>
2021-01-25 18:28:45 +0900

    finished

--- node_server/client/server.js
+++ node_server/client/server.js
@@ -3,39 +3,61 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
-
+const port = process.env.PORT ||4000;
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-
-
-const data  = fs.readFileSync('./database.json');
-const conf  = JSON.parse(data);
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
 const mysql = require('mysql');
-const connection = mysql.createConnection({
-  host:conf.host,
-  user:conf.user,
-  password:conf.password,
-  port:conf.port,
-  database:conf.database
-});
 
-connection.connect(err=>{
-  console.log(err);
+const multer = require('multer');
+const upload = multer({dest: './upload'})
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
 });
+connection.connect();
 
 app.get('/api/customers', (req, res) => {
-  connection.query('select * from customer',(err,results)=>{
-    if(err){
-      console.log(err);
-    }
-    else{
-      res.send(results);
-      console.log(results);
-    }
-  });
+    connection.query(
+        'SELECT * FROM customer WHERE isDeleted = 0',
+        (err, rows, fields) => {
+            res.send(rows);
+            console.log(err);
+        }
+    )
 });
 
-app.listen(4000, () => {
-  console.log('server is loading');
-}); 
+app.use('/image', express.static('./upload'));
+
+app.post('/api/customers', upload.single('image'), (req, res) => {
+    let sql = 'INSERT INTO customer VALUES (null, ?, ?, ?, ?, ?, now(), 0)';
+    let image = 'http://localhost:4000/image/' + req.file.filename;
+    let name = req.body.name;
+    let birthday = req.body.birthday;
+    let gender = req.body.gender;
+    let job = req.body.job;
+    let params = [image, name, birthday, gender, job];
+    connection.query(sql, params,
+        (err, rows, fields) => {
+            res.send(rows);
+        }
+    )
+});
+
+app.delete('/api/customers/:id', (req, res) => {
+    let sql = 'UPDATE customer SET isDeleted = 1 WHERE id = ?';
+    let params = [req.params.id];
+    connection.query(sql, params,
+        (err, rows, fields) => {
+            res.send(rows);
+        }
+    )
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
